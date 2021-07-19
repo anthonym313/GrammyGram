c6004e3e9a281eba1192c8a1cb615291bddb7b8c
Joe <Joe.zhang229@gmail.com>
2021-07-19 15:09:48 -0700

    removed fontawesome

--- react-app/src/components/Navigation/NavBar.js
+++ react-app/src/components/Navigation/NavBar.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from '../auth/LogoutButton';
@@ -13,43 +12,54 @@ const NavBar = () => {
 	return (
 		<nav>
 			<ul>
-				<li>
-					<NavLink to='/' exact={true} activeClassName='active'>
-						<img src='outline_home_black_48dp_1x.png'></img>
-					</NavLink>
-				</li>
-				{user ? null : (
-					<li>
-						<NavLink
-							to='/login'
-							exact={true}
-							activeClassName='active'
-						>
-							Login
+				<div className='navbar left'>
+					<li className='navbar logo'>
+						<NavLink to='/' exact={true} activeClassName='active'>
+							<img src='grammygramlogo.png'></img>
 						</NavLink>
 					</li>
-				)}
-				{user ? null : (
-					<li>
-						<NavLink
-							to='/sign-up'
-							exact={true}
-							activeClassName='active'
-						>
-							Sign Up
+				</div>
+				<div className='navbar right'>
+					<li className='navbar home'>
+						<NavLink to='/' exact={true} activeClassName='active'>
+							<img
+								src='outline_home_black_48dp_1x.png'
+								alt='Home'
+							></img>
 						</NavLink>
 					</li>
-				)}
-
-				{user ? (
-					<li>
-						<LogoutButton />
-					</li>
-				) : (
-					<li>
-						<DemoUser />
-					</li>
-				)}
+					{user ? null : (
+						<li>
+							<NavLink
+								to='/login'
+								exact={true}
+								activeClassName='active'
+							>
+								Login
+							</NavLink>
+						</li>
+					)}
+					{user ? null : (
+						<li>
+							<NavLink
+								to='/sign-up'
+								exact={true}
+								activeClassName='active'
+							>
+								Sign Up
+							</NavLink>
+						</li>
+					)}
+					{user ? (
+						<li>
+							<LogoutButton />
+						</li>
+					) : (
+						<li>
+							<DemoUser />
+						</li>
+					)}
+				</div>
 			</ul>
 		</nav>
 	);
