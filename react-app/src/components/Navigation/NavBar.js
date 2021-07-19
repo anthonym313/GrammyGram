b8d472518cefb67ac0368c5200fc086528204d55
Anthony <anthony.a.s.martin@gmail.com>
2021-07-19 16:57:22 -0400

    factor navbar to show buttons conditionally

--- react-app/src/components/Navigation/NavBar.js
+++ react-app/src/components/Navigation/NavBar.js
@@ -1,5 +1,6 @@
 
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from '../auth/LogoutButton';
 import DemoUser from '../DemoUser';
@@ -7,7 +8,8 @@ import './Navigation.css'
 
 
 const NavBar = () => {
-  
+  const user = useSelector((state) => state.session.user);
+
   return (
     <nav>
       <ul>
@@ -16,27 +18,20 @@ const NavBar = () => {
             Home
           </NavLink>
         </li>
+        {(user)?null: 
         <li>
           <NavLink to='/login' exact={true} activeClassName='active'>
             Login
           </NavLink>
-        </li>
-        <li>
+        </li>}
+        {(user)?null:<li>
           <NavLink to='/sign-up' exact={true} activeClassName='active'>
             Sign Up
           </NavLink>
-        </li>
-        <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
-            Users
-          </NavLink>
-        </li>
-        <li>
-          <DemoUser/>
-        </li>
-        <li>
-          <LogoutButton />
-        </li>
+        </li>}
+        
+        {(user)?<li><LogoutButton /></li>:<li><DemoUser/></li>}
+        
       </ul>
     </nav>
   );
