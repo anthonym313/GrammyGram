d59e2655befc1d3f08e85952acdc5f217a80104d
David Mejia <david7mejia@gmail.com>
2021-07-19 14:45:07 -0500

    fixed database

--- app/models/comment.py
+++ app/models/comment.py
@@ -5,9 +5,11 @@ class Comment(db.Model):
     __tablename__ = 'comments'
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer)
-    image_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    image_id = db.Column(db.Integer, db.ForeignKey("images.id"))
     comment = db.Column(db.TEXT)
+
+    users = db.relationship('User', back_populates='comments')
 
     def to_dict(self):
         return {
--- app/models/image.py
+++ app/models/image.py
@@ -5,9 +5,11 @@ class Image(db.Model):
     __tablename__ = 'images'
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     image_url = db.Column(db.VARCHAR(300), nullable=False)
     description = db.Column(db.VARCHAR(500), nullable=False)
+
+    users = db.relationship('User', back_populates='images')
 
     def to_dict(self):
         return {
