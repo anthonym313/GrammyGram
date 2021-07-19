baef75f13f4c45b58cad395cb7adf606a8d32d8a
Joe <Joe.zhang229@gmail.com>
2021-07-19 15:57:52 -0700

    explore icon and logged in navbar positioning

--- react-app/src/components/Navigation/NavBar.js
+++ react-app/src/components/Navigation/NavBar.js
@@ -9,9 +9,58 @@ import './Navigation.css';
 const NavBar = () => {
 	const user = useSelector((state) => state.session.user);
 
+	let sessionLinks;
+	if (user) {
+		sessionLinks = (
+			<>
+				<li className='navbar space'></li>
+				<li>
+					<NavLink to='/' exact={true} activeClassName='active'>
+						<img
+							src='outline_home_black_48dp_1x.png'
+							alt='Home'
+						></img>
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to='/' exact={true} activeClassName='active'>
+						<img
+							src='outline_explore_black_48dp_1x.png'
+							alt='Explore'
+						></img>
+					</NavLink>
+				</li>
+				<li>
+					<LogoutButton />
+				</li>
+			</>
+		);
+	} else {
+		sessionLinks = (
+			<>
+				<li>
+					<NavLink to='/login' exact={true} activeClassName='active'>
+						Login
+					</NavLink>
+				</li>
+				<li>
+					<NavLink
+						to='/sign-up'
+						exact={true}
+						activeClassName='active'
+					>
+						Sign Up
+					</NavLink>
+				</li>
+				<li>
+					<DemoUser />
+				</li>
+			</>
+		);
+	}
 	return (
-		<nav>
-			<ul>
+		<nav className='navbar'>
+			<ul className='navbar navlinks'>
 				<div className='navbar left'>
 					<li className='navbar logo'>
 						<NavLink to='/' exact={true} activeClassName='active'>
@@ -19,47 +68,7 @@ const NavBar = () => {
 						</NavLink>
 					</li>
 				</div>
-				<div className='navbar right'>
-					<li className='navbar home'>
-						<NavLink to='/' exact={true} activeClassName='active'>
-							<img
-								src='outline_home_black_48dp_1x.png'
-								alt='Home'
-							></img>
-						</NavLink>
-					</li>
-					{user ? null : (
-						<li>
-							<NavLink
-								to='/login'
-								exact={true}
-								activeClassName='active'
-							>
-								Login
-							</NavLink>
-						</li>
-					)}
-					{user ? null : (
-						<li>
-							<NavLink
-								to='/sign-up'
-								exact={true}
-								activeClassName='active'
-							>
-								Sign Up
-							</NavLink>
-						</li>
-					)}
-					{user ? (
-						<li>
-							<LogoutButton />
-						</li>
-					) : (
-						<li>
-							<DemoUser />
-						</li>
-					)}
-				</div>
+				<div className='navbar right'>{sessionLinks}</div>
 			</ul>
 		</nav>
 	);
