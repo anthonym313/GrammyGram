467019c5ba1b14dd23e2d6d7f6880bf6f3bdfd64
David Mejia <david7mejia@gmail.com>
2021-07-20 18:40:29 -0500

    add links to post and upload

--- react-app/src/components/Feed/index.js
+++ react-app/src/components/Feed/index.js
@@ -2,16 +2,15 @@ import React, { useEffect } from "react";
 // import { NavLink } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { getImagesThunk } from "../../store/post";
+import "./feed.css";
 
 function Feed() {
   const dispatch = useDispatch();
 
-  const allPosts = useSelector(state => Object.values(state.feedPosts));
+  const allPosts = useSelector((state) => Object.values(state.feedPosts));
 
-  const pureIm = allPosts[0].posts
+  const pureIm = allPosts[0].posts;
   // console.log(`###########################`,allPosts[0].posts)
-
-
 
   useEffect(() => {
     dispatch(getImagesThunk());
@@ -20,14 +19,21 @@ function Feed() {
   return (
     <div>
       {pureIm &&
-        pureIm?.map(image => (
-          <div key={image.id}>
-            <li>{image.id}</li>
+        pureIm?.map((image) => (
+          <div key={image.id} className="post-container">
+            <div className="top-bar"> </div>
+            <div className="image-container">
+              <img src={image?.image_url} alt="feed-images" className='' />
+            </div>
+            <div className="post-content">
+              <div className="post-actions"></div>
+              <div className="comments"></div>
+              <div classname="post-comment"> </div>
+            </div>
           </div>
-        ))
-      }
+        ))}
     </div>
-        )
-    }
+  );
+}
 
 export default Feed;
--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,31 +1,40 @@
-
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import LogoutButton from './auth/LogoutButton';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import LogoutButton from "./auth/LogoutButton";
 
 const NavBar = () => {
   return (
     <nav>
       <ul>
         <li>
-          <NavLink to='/' exact={true} activeClassName='active'>
+          <NavLink to="/" exact={true} activeClassName="active">
             Home
           </NavLink>
         </li>
         <li>
-          <NavLink to='/login' exact={true} activeClassName='active'>
+          <NavLink to="/login" exact={true} activeClassName="active">
             Login
           </NavLink>
         </li>
         <li>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
+          <NavLink to="/sign-up" exact={true} activeClassName="active">
             Sign Up
           </NavLink>
         </li>
         <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
+          <NavLink to="/users" exact={true} activeClassName="active">
             Users
           </NavLink>
+          <li>
+          <NavLink to="/posts/" exact={true} activeClassName="active">
+            Feed
+          </NavLink>
+          </li>
+          <li>
+          <NavLink to="/posts/upload" exact={true} activeClassName="active">
+            Post
+          </NavLink>
+          </li>
         </li>
         <li>
           <LogoutButton />
@@ -33,6 +42,6 @@ const NavBar = () => {
       </ul>
     </nav>
   );
-}
+};
 
 export default NavBar;
