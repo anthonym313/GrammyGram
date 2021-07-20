0cc5deca88b1b28c4371c985422956d7a171684c
Joe <Joe.zhang229@gmail.com>
2021-07-20 16:20:29 -0700

    thunk

--- react-app/src/components/CommentList/index.js
+++ react-app/src/components/CommentList/index.js
@@ -1,29 +1,31 @@
 import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 
 const CommentList = () => {
+	const [user, setUser] = useState([]);
 	const [comment, setComments] = useState([]);
+	const allComments = useSelector((state) => state.session.comment);
 
 	// {for comment in comments}
 	const commentComponents = comment.map((user) => {
 		return (
-			// <li key={user.id}>
-			// 	<NavLink to={`/users/${user.id}`}>{user.username}</NavLink>
-			// </li>
 			<ul>
 				<li key={user.id}>
 					<NavLink to={`/users/${user.id}`}>{user.username}</NavLink>
 				</li>
-				<li key={comment.id}></li>
+				<li key={comment.id}>{comment.comment}</li>
 			</ul>
 		);
 	});
 
+	console.log('all Comments', allComments);
+
 	return (
-		<>
+		<div className='commentList-container'>
 			<h3>Comments: </h3>
 			<ul>{commentComponents}</ul>
-		</>
+		</div>
 	);
 };
 
