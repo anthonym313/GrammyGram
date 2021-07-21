a755fb0c83f254153cf150de608db844be9ebe0d
Joe <Joe.zhang229@gmail.com>
2021-07-21 09:17:25 -0700

    no changes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -12,48 +12,50 @@ import Feed from './components/Feed';
 import { authenticate } from './store/session';
 
 function App() {
-  const [loaded, setLoaded] = useState(false);
-  const dispatch = useDispatch();
+	const [loaded, setLoaded] = useState(false);
+	const dispatch = useDispatch();
 
-  useEffect(() => {
-    (async () => {
-      await dispatch(authenticate());
-      setLoaded(true);
-    })();
-  }, [dispatch]);
+	useEffect(() => {
+		(async () => {
+			await dispatch(authenticate());
+			setLoaded(true);
+		})();
+	}, [dispatch]);
 
-  if (!loaded) {
-    return null;
-  }
+	if (!loaded) {
+		return null;
+	}
 
-  return (
-    <BrowserRouter>
-      <NavBar />
-      <Switch>
-        <Route path='/login' exact={true}>
-          <LoginForm />
-        </Route>
-        <Route path='/sign-up' exact={true}>
-          <SignUpForm />
-        </Route>
-        <ProtectedRoute path='/users' exact={true} >
-          <UsersList />
-        </ProtectedRoute>
-        <ProtectedRoute path='/posts' exact={true} >
-          <Feed />
-        </ProtectedRoute>
-        <ProtectedRoute path='/posts/upload' exact={true} >
-          <PostForm />
-        </ProtectedRoute>
-        <ProtectedRoute path='/users/:userId' exact={true} >
-          <User />
-        </ProtectedRoute>
-        <ProtectedRoute path='/' exact={true} >
-          {/* <Feed/> */}
-        </ProtectedRoute>
-      </Switch>
-    </BrowserRouter>
-  );
+	// no changes
+
+	return (
+		<BrowserRouter>
+			<NavBar />
+			<Switch>
+				<Route path='/login' exact={true}>
+					<LoginForm />
+				</Route>
+				<Route path='/sign-up' exact={true}>
+					<SignUpForm />
+				</Route>
+				<ProtectedRoute path='/users' exact={true}>
+					<UsersList />
+				</ProtectedRoute>
+				<ProtectedRoute path='/posts' exact={true}>
+					<Feed />
+				</ProtectedRoute>
+				<ProtectedRoute path='/posts/upload' exact={true}>
+					<PostForm />
+				</ProtectedRoute>
+				<ProtectedRoute path='/users/:userId' exact={true}>
+					<User />
+				</ProtectedRoute>
+				<ProtectedRoute path='/' exact={true}>
+					{/* <Feed/> */}
+				</ProtectedRoute>
+			</Switch>
+		</BrowserRouter>
+	);
 }
 
 export default App;
