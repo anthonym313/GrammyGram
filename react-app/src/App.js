fa74950c98789ef095fa3c804a060da53cb5daf3
David Mejia <david7mejia@gmail.com>
2021-07-22 12:02:01 -0500

    css1

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -58,7 +58,7 @@ function App() {
         <ProtectedRoute path="/explore" exact={true}>
           <ExplorePage />
         </ProtectedRoute>
-        <ProtectedRoute path="/posts/postId" exact={true}>
+        <ProtectedRoute path="/posts/:postId" exact={true}>
           <SinglePost />
         </ProtectedRoute>
         <Route component={NotFoundPage} />
