a7d55790e2c57259ef113d79052ac0a84c381d14
David Mejia <david7mejia@gmail.com>
2021-07-23 16:05:00 -0500

    style feed

--- react-app/src/components/Suggestions/index.js
+++ react-app/src/components/Suggestions/index.js
@@ -1,50 +1,56 @@
-import React from 'react'
-import {useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import './Suggestions.css'
-import {useSelector} from "react-redux"
-import { getFiveRandomUsers } from '../../store/user';
+import React from "react";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import "./Suggestions.css";
+import { useSelector } from "react-redux";
+import { getFiveRandomUsers } from "../../store/user";
 
-export default function Suggestions(){
-    const user = useSelector((state)=>state.session.user)
-    const suggestions = useSelector((state) => state.randomUsers)
-    const dispatch = useDispatch()
+export default function Suggestions() {
+  const user = useSelector((state) => state.session.user);
+  const suggestions = useSelector((state) => state.randomUsers);
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(getFiveRandomUsers())
+  useEffect(() => {
+    dispatch(getFiveRandomUsers());
+  }, [dispatch]);
 
-    }, [dispatch]);
-
-
-
-    return(
-            <div className="UserSide-container">
-                    <div className='user-avi_small'>
-                        {/* <img src={user.avatar} alt='small avi'></img> */}
-                    </div>
-                    <div>
-                        <h3>{user.username}</h3>
-                    </div>
-                    <h2>Suggestions For You</h2>
-                    {suggestions?.map((randomUser)=>{
-                        return(
-                            <div>
-                                <div>
-                                    <img src={randomUser.avatar} alt='random-user'></img>
-                                    <h3>{randomUser.username}</h3>
-                                </div>
-
-
-
-                            </div>
-
-                        )
-                    })}
-
-
+  return (
+    <div className="UserSide-container">
+      <div className="user-avi_small"></div>
+      <div className="random-holder">
+        {suggestions?.map((randomUser) => {
+          return (
+            <div className="random-div">
+              <img
+                src={randomUser.avatar}
+                className="random-usr"
+                alt="random-user"
+              ></img>
+              <p className="random-username">
+                {randomUser.username.substring(0, 7) + "..."}
+              </p>
             </div>
-
-
-    )
+          );
+        })}
+      </div>
+      <div className="user-profile-holder">
+        <img src={user.avatar} alt="small avi" className="user-avi"></img>
+        <p className="profile-user">{user.username}</p>
+        {/* {suggestions?.map((randomUser) => {
+          return (
+            <div className="random-div">
+              <img
+                src={randomUser.avatar}
+                className="random-usr"
+                alt="random-user"
+              ></img>
+              <p className="random-username">
+                {randomUser.username.substring(0, 7) + "..."}
+              </p>
+            </div>
+          );
+        })} */}
+      </div>
+    </div>
+  );
 }
-
