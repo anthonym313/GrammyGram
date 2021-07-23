963481e44f70c9fbc1536a52193195a40911dae6
Anthony <anthony.a.s.martin@gmail.com>
2021-07-23 19:27:45 -0400

    redirect default home route to post route

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import LoginForm from './components/auth/LoginForm';
 import SignUpForm from './components/auth/SignUpForm';
@@ -8,7 +8,6 @@ import ProtectedRoute from './components/auth/ProtectedRoute';
 import UsersList from './components/UsersList';
 import User from './components/User';
 import PostForm from './components/PostForm';
-import CommentList from './components/CommentList';
 import Feed from './components/Feed';
 import NotFoundPage from './components/NotFoundPage';
 import ExplorePage from './components/ExplorePage';
@@ -39,6 +38,9 @@ function App() {
 				</Route>
 				<Route path='/sign-up' exact={true}>
 					<SignUpForm />
+				</Route>
+				<Route path='/' exact={true}>
+					<Redirect to='/posts/'/>
 				</Route>
 				<ProtectedRoute path='/users' exact={true}>
 					<UsersList />
