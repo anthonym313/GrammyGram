a6974540004777548276847f6353d7b1bf957572
Anthony <anthony.a.s.martin@gmail.com>
2021-08-25 17:33:04 -0400

    use font awesome icons

--- react-app/src/components/UserProfile/index.js
+++ react-app/src/components/UserProfile/index.js
@@ -3,8 +3,10 @@ import { useParams } from 'react-router-dom';
 import { useDispatch,useSelector } from 'react-redux';
 import {singleUser} from '../../store/user'
 
+import './UserProfile.css'
+
 function UserProfile() {
-	const user = useSelector((state)=> state.singleUser.user)
+	const user = useSelector((state)=> state.singleUser.user) || null
 	const { userId } = useParams();
 	const dispatch = useDispatch();
 	
@@ -13,22 +15,21 @@ function UserProfile() {
 		
 	}, [dispatch,userId]);
 
-	if (!user) {
-		return null;
-	}
 
-	return (
-		<ul>
-			<li>
-				<strong>User Id</strong> {userId}
-			</li>
-			<li>
-				<strong>Username</strong> {user.username}
-			</li>
-			<li>
-				<strong>Email</strong> {user.email}
-			</li>
-		</ul>
+	return user &&(
+		<div className='userProfile-container'>
+			<div className='userProfile'>
+				<div className='profile-image'>
+					<img src={user.avatar} alt='userProfile photo'></img>
+				</div>
+				<div className='userProfile-settings'>
+					<h1 className='userProfile-username'>{user.username}</h1>
+					<button id='edit-userProfile'>Edit Profile</button>
+					<span className="fas fa-cog"></span>
+				</div>
+
+			</div>
+		</div>
 	);
 }
 export default UserProfile;
