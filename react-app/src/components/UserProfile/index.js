fdff1bf8626fb9771d805c22848441595677aa71
Anthony <anthony.a.s.martin@gmail.com>
2021-08-25 17:43:31 -0400

    add place for profile stats

--- react-app/src/components/UserProfile/index.js
+++ react-app/src/components/UserProfile/index.js
@@ -9,6 +9,11 @@ function UserProfile() {
 	const user = useSelector((state)=> state.singleUser.user) || null
 	const { userId } = useParams();
 	const dispatch = useDispatch();
+
+	const [postCount, setPostCount]=useState(0)
+	const [followerCount, setFollowerCount]=useState(0)
+	const [followingCount, setFollowingCount]=useState(0)
+	
 	
 	useEffect(() => {
 		dispatch(singleUser(userId))
@@ -26,6 +31,11 @@ function UserProfile() {
 					<h1 className='userProfile-username'>{user.username}</h1>
 					<button id='edit-userProfile'>Edit Profile</button>
 					<span className="fas fa-cog"></span>
+				</div>
+				<div className='userProfile-stats'>
+					<span>{postCount} posts </span>
+					<span>{followerCount} followers </span>
+					<span>{followingCount} following </span>
 				</div>
 
 			</div>
